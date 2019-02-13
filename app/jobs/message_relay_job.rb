c341e9eb8658a176310e55479dbf12fad1a3dcdf
Nikhil Hira <nikhil@leancode.io>
2019-02-13 15:51:13 -0800

    add notification when user is not on chat page and receives a new message

--- app/jobs/message_relay_job.rb
+++ app/jobs/message_relay_job.rb
@@ -3,7 +3,10 @@ class MessageRelayJob < ApplicationJob
 
   def perform(message)
     ActionCable.server.broadcast "chatrooms: #{message.chatroom.id}",
-      { message: MessagesController.render(message),
-        chatroom_id: message.chatroom.id }
+      { 
+        username: message.user.username,
+        body: message.body,
+        chatroom_id: message.chatroom.id 
+      }
   end
 end
